2abd7c9876a1a5c97c9c6478d37580c53ce1ae8e
jyotheesh k <jyothishc30@gmail.com>
2022-09-29 11:45:09 +0530

    new-app-login-details-api

---
+++ src/main/java/com/example/demo/NewAppLoginDetailsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAppLoginDetailsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewAppLoginDetailsApiApplication.class, args);
+	}
+
+}
